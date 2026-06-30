e93346c55dc8ad7405742dea864a54274c7cfb4e
Iyiowo <okoyechiagoziem2017@gmail.com>
2026-06-30 17:16:29 +0100

    framework

--- src/App.js
+++ src/App.js
@@ -1,3 +1,5 @@
+import { useState } from "react";
+
 function App() {
   return (
     <div className="App">
@@ -5,4 +7,34 @@ function App() {
     </div>
   );
 }
+export default App;
+importREact, { useState } from 'react';
+import { TextField, Button } from '@mui/material';
+import './App.css';
+function App() {
+  const [todos, setTodos] = useState([
+    'Create New Bank Account',
+    'Read on Web Development',
+    'Rest'
+  ]);
+  const [input, setInput] = useState('');
+  const addTodo = (e) => {
+    e.preventDefault();
+    setTodos([...todos, input]);
+    setInput('')
+  };
+
+  return (
+    <div>
+      <h2>TODO App</h2>
+      <form>
+        <TextField id="outlined-basic" label="Make Todo" variant="outlined" style={{ margin: "0px 5px" }} size="small" value={input} onChange={e => setInput(e.target.value)} />
+        <button variant="contained" color="primary" onClick={addTodo} >Add Todo</button>
+      </form>
+      <ul>
+        {todos.map(todo => <li></li>)}
+      </ul>
+    </div>
+  );
+};
 export default App;
