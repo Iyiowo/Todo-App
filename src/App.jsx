b8165d867db0b10f131d542c2187f5903c462960
Iyiowo <okoyechiagoziem2017@gmail.com>
2026-07-06 21:44:47 +0100

    added components folder with 2 new files

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,24 @@
+import { AddTaskComponent } from "./components/AddTaskComponent";
+import { NavbarComponent } from "./components/NavbarComponent";
+
 function App() {
   return (
-    <>
-      <h1 className="text-orange-500 underline">To do list application</h1>
-      <button className="btn btn-primary">Primary</button>
-    </>
+    <div className="h-full">
+      {/* navbar */}
+      <nav>Navbar component</nav>
+
+      {/* task container */}
+      <div className="flex w-full h-full gap-8 py-6 px-6">
+        {/* add task components */}
+        <div className="bg-green-100 rounded-md p-4">Add Task Components</div>
+
+        {/* my task component */}
+        <div className="flex flex-col gap-3 flex-3/4 bg-green-100 rounded-md p-4">
+          <h1 className="text-green-900 font-semibold text-lg">My Tasks</h1>
+          <div>My Task Component</div>
+        </div>
+      </div>
+    </div>
   );
 }
 export default App;
---
+++ src/components/AddTaskComponent.jsx
@@ -0,0 +1,34 @@
+
+const AddTaskComponent = () => {
+  return (
+    <>
+      <div className="flex flex-1/4 flex-col gap-2 bg-green-100 rounded-md p-4">
+        <h1 className="texte-green-900 font-semibold text-lg">Add Your Task</h1>
+        <form className="flex flex-col gap-3">
+          <input
+            type="text"
+            id="taskTitle"
+            placeholder="Task Title..."
+            className="p-3 w-full text-white rounded-md border-gray-100 bg-green-800 shadow-xs text-sm placeholder:text-gray-300"
+          />
+
+          <textarea
+            id="taskDesc"
+            cols={30}
+            rows={12}
+            className="p-3 rounded-md text-sm w-full text-white border-gray-100 bg-green-800 placeholder:text-gray-300 resize-none"
+            placeholder="Write your task here..."
+          ></textarea>
+
+          <button
+            type="submit"
+            className="btn w-full px-4 py-2 text-sm text-white rounded-md bg-green-600 hover:bg-green-800 transition ease-in-out">
+            Save
+          </button>
+        </form>
+      </div>
+    </>
+  );
+};
+
+export default AddTaskComponent;
