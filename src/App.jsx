97ad6ef3a29ea9479cd4ca033b816b859f129914
Iyiowo <okoyechiagoziem2017@gmail.com>
2026-07-06 18:34:06 +0100

    daisyui and tailwind added

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,8 @@
 function App() {
   return (
     <>
-      <h1>To do list application</h1>
+      <h1 className="text-orange-500 underline">To do list application</h1>
+      <button className="btn btn-primary">Primary</button>
     </>
   );
 }
